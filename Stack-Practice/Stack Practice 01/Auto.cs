80674fbfd9981613f6d5f00446a075e7d8bc822e
Juan Beltrame <juan_beltrame@hotmail.com>
2023-12-16 13:05:06 -0300

    Update

---
+++ Stack-Practice/Stack Practice 01/Auto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Practice_01
+{
+    internal class Auto
+    {
+        int modelo;
+        int patente;
+        Color color;
+        decimal cantKm;
+
+
+        public Auto(int modelo, int patente, Color color, decimal cantidadKilometros)
+        {
+            this.modelo = modelo;
+            this.patente = patente;
+            this.color = color;
+            cantKm = cantidadKilometros;
+        }
+
+        public string GetCarInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Informacion del Auto dentro del Garage");
+            sb.AppendLine($"Modelo: {modelo.ToString()}");
+            sb.AppendLine($"Patente: {patente.ToString()}");
+            sb.AppendLine($"Color: {color.ToString()}");
+            sb.AppendLine($"Cantidad de Kilometos: {cantKm.ToString()}");
+
+            return sb.ToString();
+        }
+    }
+}
--- Stack-Practice/Stack Practice 01/Program.cs
+++ Stack-Practice/Stack Practice 01/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Stack_Practice_01;
+using System.Drawing;
+
+// Las colecciones en C# son objetos, por lo tanto ademas, sin tipos de referencia. 
+
+// Forma 1: Declaracion y asignacion en una Linea sin valores iniciales
+Stack<int> pila01 = new Stack<int>();
+
+// Forma 2: Declaracion y asignacion en una linea sin valores iniciales con capacidad inicial especificada
+Stack<int> pila02 = new Stack<int>(10);
+
+// Forma 3: Declaracion e inicializacion en una sola linea con elementos
+// Esta forma no se puede, o bien desconozoco como hacerlo
+
+// Forma 4: Declaracion y asignacion en varias Lineas
+Stack<int> pila04; 
+pila04 = new Stack<int>();
+
+// ---------------------------------------------------------------------------------------------------------
+// Forma 1: Declarar Objetos // Declaracion y asignacion en una Linea sin valores iniciales
+Stack<Auto> autoPila01 = new Stack<Auto>();
+
+// Forma 2: Declarar Objetos // Declaracion y asignacion en una linea sin valores inciales con capacidad inicial especificada
+Stack<Auto> autoPila02 = new Stack<Auto>(10);
+
+// Forma 3: Declarar Objetos // Declaracion e inicializacion en una sola linea con elementos
+Stack<Auto> autoPila03 = new Stack<Auto>(new Auto[] 
+{ 
+   new Auto(1, 12343, Color.AliceBlue, 1000), 
+   new Auto(1, 12343, Color.AliceBlue, 1000), 
+   new Auto(1, 12343, Color.AliceBlue, 1000) 
+});
+
+// Forma 4: Declarar Objetos // Delcaracion y asignacion en varias Lineas
+Stack<Auto> autoPila04;
+autoPila04 = new Stack<Auto>();
