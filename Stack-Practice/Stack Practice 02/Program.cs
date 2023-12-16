f7d688ff9f5228ef6b9da6b17cbc47702db60930
Juan Beltrame <juan_beltrame@hotmail.com>
2023-12-16 16:00:10 -0300

    Update

---
+++ Stack-Practice/Stack Practice 02/Program.cs
@@ -0,0 +1,28 @@
+﻿Stack<string> paises = new Stack<string>();
+
+Console.WriteLine("-------------");
+paises.Push("Argentina");
+paises.Push("Brasil");
+paises.Push("Peru");
+paises.Push("Bolivia");
+foreach(string item in paises)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine("-------------");
+// El Metodo Pop(). Elimina el ultimo elemento en salir
+paises.Pop();
+foreach (string item in paises)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine("-------------");
+// El Metodo Peek(). Devuelve el elemento sin eliminarlo 
+Console.WriteLine(paises.Peek());
+Console.WriteLine("-------------");
+foreach (string item in paises)
+{
+    Console.WriteLine(item);
+}
+
+
---
+++ Stack-Practice/Stack Practice 03/Program.cs
@@ -0,0 +1,37 @@
+﻿using Stack_Practice_03;
+using System.Drawing;
+
+Auto auto01 = new Auto(1, 12, Color.Tan, 100);
+Auto auto02 = new Auto(2, 12, Color.Tan, 100);
+Auto auto03 = new Auto(3, 12, Color.Tan, 100);
+
+Stack<Auto> autosPila = new Stack<Auto>();
+
+Console.WriteLine("--------------------------------");
+autosPila.Push(auto01);
+autosPila.Push(auto02);
+autosPila.Push(auto03);
+foreach (var item in autosPila)
+{
+    Console.WriteLine(item.GetCarInfo());
+}
+Console.WriteLine("--------------------------------");
+Console.WriteLine(autosPila.Peek().GetCarInfo());
+Console.WriteLine("--------------------------------");
+Console.WriteLine(autosPila.Pop().GetCarInfo());
+Console.WriteLine("--------------------------------");
+foreach (var item in autosPila)
+{
+    Console.WriteLine(item.GetCarInfo());
+}
+Console.WriteLine("--------------------------------");
+if (autosPila.Contains(auto01))
+{
+    Console.WriteLine("Esta en la lista");
+}
+else
+{
+    Console.WriteLine("No esta en la lista");
+}
+
+
